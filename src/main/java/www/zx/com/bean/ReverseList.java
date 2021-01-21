6cb954e12f506ddf5e0b9fe853f62f5fb8369b07
鑫 赵 <zhaoxin01@missfresh.cn>
2021-01-21 10:58:03 +0800

    啊

--- src/main/java/www/zx/com/bean/ReverseList.java
+++ src/main/java/www/zx/com/bean/ReverseList.java
@@ -25,7 +25,7 @@ public class ReverseList {
 
     }
    static ListNode tail = null; // 后驱节点
-    private static ListNode reverse(ListNode listNode,int n) {
+    private static ListNode reverseN(ListNode listNode,int n) {
         if(listNode==null)
         {
             return null;
@@ -35,10 +35,23 @@ public class ReverseList {
             tail=listNode.next;
             return listNode;
         }
-        ListNode head=reverse(listNode.next,n-1);
+        ListNode head=reverseN(listNode.next,n-1);
         listNode.next.next=listNode;
         listNode.next=tail;
         return head;
+
+    }
+    private static ListNode reverseBetween(ListNode listNode,int m,int n) {
+        if(listNode==null)
+        {
+            return null;
+        }
+        if(m==1)
+        {
+            return reverseN(listNode,n);
+        }
+        listNode.next=reverseBetween(listNode.next,m-1,n-1);
+        return listNode;
 
     }
 
@@ -47,14 +60,16 @@ public class ReverseList {
         data.next = new ListNode(1);
         data.next.next = new ListNode(2);
         data.next.next.next = new ListNode(3);
+        data.next.next.next.next = new ListNode(4);
+        data.next.next.next.next.next = new ListNode(5);
 
         ListNode test=data;
-        while (test != null)
-        {
-            System.out.println(test.val);
-            test = test.next;
-        }
-        ListNode head=reverse(data,3);
+        //while (test != null)
+        //{
+        //    System.out.println(test.val);
+        //    test = test.next;
+        //}
+        ListNode head=reverseBetween(data,3,4);
 
         while (head != null)
         {
